5ad5d13e8132f3f2251abfd9b148b6a4b3aa8ee7
Giran Oh <giranoh12@gmail.com>
2026-03-28 00:42:44 +0900

    01_dp_fibonacci, 02_dp_stairs, 피보나치 수 2

--- week5/basic/01_dp_fibonacci.py
+++ week5/basic/01_dp_fibonacci.py
@@ -72,6 +72,7 @@ DP가 필요한 경우:
 - 자원 할당
 """
 
+
 def fibonacci_memo(n, memo=None):
     """
     메모이제이션을 사용한 피보나치 (하향식 DP)
@@ -84,16 +85,20 @@ def fibonacci_memo(n, memo=None):
         n번째 피보나치 수
     """
     # TODO: memo가 None이면 빈 딕셔너리로 초기화
-    pass
+    if not memo:
+        memo = {}
     
     # TODO: base case 
-    pass
+    if n <= 2:
+        return 1
     
     # TODO: 이미 계산한 값이 memo에 있으면 반환
-    pass
-    
+    if n in memo:
+        return memo[n]
+
     # TODO: 재귀 호출하여 계산하고 memo에 저장
-    pass
+    else:
+        memo[n] = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo) # 현재 딕셔너리를 넘김
     
     return memo[n]
 
--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,28 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input())
+    def fib(n, memo=None):
+        # 처음 호출이냐(None)만 확인하고 싶을 때: memo is None
+        # 비어있거나 없을때(None, [], {}, "", 0)를 전부 처리하고 싶을 때: not memo
+        if memo is None:
+            memo = {}
+        if n in memo:
+            return memo[n]
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            memo[n] = fib(n-1, memo) + fib(n-2, memo)
+
+        return memo[n]
+    
+    print(fib(n))
+    
+if __name__ == "__main__":
+    main()
